d47648f4995ed66aa16a1d86786c9e05484cf258
speence <spence1994@hotmail.com>
2016-01-25 18:53:38 -0500

    Testing reading ppm

--- assignment1/processimage.c
+++ assignment1/processimage.c
@@ -6,10 +6,18 @@ void processImage(int width, int height, RGB *image)
   // simple image "processing"
   int i;
 
-  for (i=0; i < width*height; i++)
+  for (i=0; i < 20; i++)
     {
-     RGB *p = image+i;
-      p->r = 150; 
-	    /*printf("%d %d %d ",p->r, p->g, p->b); */
+      RGB *start = image-width+i;
+      RGB *p = image+i;
+      //p->g = 0; 
+	    printf("%d %d %d ",p->r, p->g, p->b); 
+      //if (p<start) 
+        //printf("Out of range");
+      //else
+        //printf("%d ", p->r);
+        //printf("%d ", p->r);
+        //printf("%d ", start);
+
     }
 }
