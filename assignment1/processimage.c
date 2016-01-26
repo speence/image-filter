b799bf2a5623196108a5709a3d2632a8b7bbd0e8
JADEES ANTON <antonj@mcmaster.ca>
2016-01-25 21:02:41 -0500

    updated logic so that it works with the new functions we made

--- assignment1/processimage.c
+++ assignment1/processimage.c
@@ -126,33 +126,68 @@ void processImage(int width, int height, RGB *image)
     
     
     
-      if (p%(width-1) == 0){ //if on the right edge of image
+	if (p%(width-1) == 0){	//if we are on the right edge of the image
+		rightZero();
+		if(top<start){	//if we are on the top right corner of the image
+			topRightZero();
+		}
+		else{
+			fetchTopRight();
+		}	
+		if(bottom>end){	//if we are ont he bottom right corner of the image
+			bottomRightZero();
+		}
+		else{
+			fetchBottomRight();
+		}		
+	}
 
+	else if (p%(width) == 0){	//if we are on the left edge of the image
+		leftZero();
+		if(top<start){	//if we are on the top left corner of the image
+			topLeftZero();
+		}
+		else{
+			fetchTopLeft();
+		}		
+		if(bottom>end){	//if we are on the bottom left corner of the image
+			bottomLeftZero();
+		}
+		else{
+			fetchBottomLeft();
+		}	
+	}
 
-        bottomrightred = 0;
-        bottomrightgreen = 0;
-        bottomrightblue = 0;
-        meandiv = meandiv - 3;
-        if (top<start){ //if in the top right corner
-          topred = 0;
-          topgreen = 0;
-          topblue = 0;
-          meandiv--;
-        }else {
-          topred = top->r;
-          topgreen = top->g;
-          topblue = top->b;
-        }
-        if (bottom>end){ //if in bottom right corner
-          bottomred = 0;
-          bottomgreen = 0;
-          bottomblue = 0;
-          meandiv--;
-        }else {
-          bottomred = bottom->r;
-          bottomgreen = bottom->g;
-          bottomblue = bottom->b;
-        }
-        process 
+	else if (p<(width)){	//if we are on the top edge of the image	
+		topZero();
+		if(topleft<start){	//if we are on the top left corner of image
+			topLeftZero();
+		}
+		else{
+			fetchTopLeft();
+		}
+		if(topright<start){	//if we are on the top right corner of image 
+			topRightZero();
+		}
+		else{
+			fetchTopRight();
+		}
+	}
+
+	else if (p > ((width*height)-width)){	//if we are on the bottom edge of the image
+		bottomZero();
+		if(bottomright>end){	//if we are on the bottom right corner of image
+			bottomRightZero();
+		}
+		else{
+			fetchBottomRight();
+		}
+		if(bottomleft>end){	//if we are on the bottom left corner of image
+			bottomLeftZero();
+		}
+		else{
+			fetchBottomLeft();
+		}
+	}
     }
 }
