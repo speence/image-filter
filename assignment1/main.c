591a5d2a41668b90e41ec290c4aa0cb6d7813932
JADEES ANTON <antonj@mcmaster.ca>
2016-01-27 22:48:10 -0500

    made changes to main to try and make it parallel

--- assignment1/main.c
+++ assignment1/main.c
@@ -6,32 +6,113 @@
 
 int main(int argc, char** argv[])
 {
-  RGB *image;
+  RGB *image, *output;
   int width, height, max;
-  int rank, size;
-  int rows;
+  int rank, p;
+  int source;
+  int *rows;
+  int offset;
+
+  MPI_Status status;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-  if(rank==0){
-    printf("Process %d is reading the image.\n", rank);
-    image = readPPM(argv[1], &width, &height, &max);
+  rows = (int*)malloc(sizeof(int)*p);
 
-    rows = (height/size);
 
-    printf("Number of rows sent to each process is %d \n", rows);
-
-    printf("Process %d is processing the image.\n", rank);
-    processImage(width, height, image);
-    printf("Process %d is writing to the image.\n", rank);
-    writePPM(argv[2], width, height, max, image);
-    printf("Process %d is freeing the image.\n", rank);
-    free(image); 
+  int i;
+  for(i=0;i<p;i++){
+    rows[i] = width/p;
+  }
+  for (i=0; i < width % p; i++){
+    rows[i]++; 
   }
 
-  printf("Hello world, I am %d of %d \n", rank, size);
+  if(rank==0){
+    image = (RGB*)malloc(width*height*sizeof(RGB));
+    output = (RGB*)malloc(width*height*sizeof(RGB));
+  }else{
+    image = (RGB*)malloc(width*rows[rank]*sizeof(RGB));
+    output = (RGB*)malloc(width*rows[rank]*sizeof(RGB));
+  }
+
+  int tag = 0;
+
+  if(rank==0){
+  
+    image = readPPM(argv[1], &width, &height, &max);
+
+    int offset = 0;
+    int dest = 1;
+
+    for(dest=1;dest<p;dest++){
+
+      offset += rows[dest-1];
+
+      MPI_Send(image + offset*width,
+      rows[dest]*width*sizeof(RGB),
+      MPI_CHAR,
+      dest,
+      tag,
+      MPI_COMM_WORLD);
+
+      printf("Process %d has sent data to process %d \n", rank, dest);
+      fflush(stdout);
+      }
+      
+  }else{
+
+    MPI_Recv(image,
+      rows[rank]*width*sizeof(RGB),
+      MPI_CHAR,
+      0,
+      tag,
+      MPI_COMM_WORLD,
+      &status);
+
+    printf("Process %d has received data from process 0 \n", rank);
+
+  }
+
+  MPI_Bcast(&width, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+  processImage(width, rows[rank]*sizeof(RGB), image);
+  printf("%d is done processing \n", rank);
+
+  //sending data back to process 0
+  if(rank!=0){
+    MPI_Send(output,
+      rows[rank],
+      MPI_CHAR,
+      0,
+      tag,
+      MPI_COMM_WORLD
+      );
+
+    writePPM(argv[2], width, height, max, image);
+
+  }else{
+    offset = 0;
+    for(source = 1; source < p; source++){
+      offset += rows[source-1];
+      MPI_Recv(output+offset*width,
+        rows[source],
+        MPI_CHAR,
+        source,
+        tag,
+        MPI_COMM_WORLD,
+        &status);
+
+      printf("0 has received data from %d \n", source);
+
+      writePPM(argv[2], width, rows[rank]*sizeof(RGB), max, image);
+
+    }
+    free(image);
+  }
+
 
   MPI_Finalize();
 
